a774f6b6a6da63a6c58130a0785f93357f306e73
IvanovVladislav <ivanov2703@mail.ru>
2022-09-08 16:38:45 +0300

    add HomeWork4

---
+++ HomeWork4.py
@@ -0,0 +1,13 @@
+print ('HomeWork4')
+n = int(input('Ведите номер четверти плоскости(1,2,3,4): '))
+while n < 1 or n > 4:
+    print('Не верно!')
+    n = int(input('Ведите номер четверти плоскости(1,2,3,4): '))
+if n == 1:
+    print('x > 0 и y > 0')
+elif n == 2:
+    print('x < 0 и y > 0')
+elif n == 3:
+    print('x < 0 и y < 0')
+elif n == 4:
+    print('x > 0 и y < 0')
